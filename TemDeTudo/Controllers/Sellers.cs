4d0c8f5e361d674f021ce3122cec3f5702c4fe6b
GABRIEL PEROTTONI CARAVANTES <85756512004@fspoa.br>
2024-09-30 21:42:55 -0300

    adicionando sellers

---
+++ TemDeTudo/Controllers/Sellers.cs
@@ -0,0 +1,9 @@
+﻿using TemDeTudo.Models;
+
+namespace TemDeTudo.Controllers
+{
+    public class Sellers
+    {
+        public Department? Department { get; internal set; }
+    }
+}
---
+++ TemDeTudo/Controllers/SellersController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TemDeTudo.Data;
+
+namespace TemDeTudo.Controllers
+{
+    public class SellersController: Controller
+    {
+        private readonly TemDeTudoContext _context;
+
+        public SellersController(TemDeTudoContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+           //List<Seller> sellers = _context.Seller.ToList();
+            var sellers = _context.Seller.Include("Department").ToList();
+            return View(sellers);
+        }
+        public IActionResult Create() {
+        return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Sellers seller)
+        {
+            //testa se foi passado um vendedor
+            if(seller == null)
+            {
+                return NotFound();
+            }
+
+            seller.Department = _context.Department.FirstOrDefault();
+
+            //adicionar o objeto vendedor ao banco
+            _context.Add(seller);
+            //salvar as alterações na base de dados
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
